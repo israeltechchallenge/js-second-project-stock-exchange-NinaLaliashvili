422901a42494d2e923a9375cf8ce07bf8483f071
Nina Laliashvili <nini@gonzalez.ge>
2023-02-18 22:07:39 +0200

    Milestone 1

---
+++ .github/scripts.js
@@ -0,0 +1,49 @@
+// Get every html element
+const searchInput = document.getElementById("searchInput");
+const searchButton = document.getElementById("searchButton");
+const searchResults = document.getElementById("searchResults");
+const theLoader = document.getElementById("loader");
+
+//create function to make my spinner visible.
+function enableSpinner() {
+  theLoader.classList.remove("invisible");
+}
+
+//create function to make my spinner unvisible
+function disableSpinner() {
+  theLoader.classList.add("invisible");
+}
+
+//create async function for my API, to display 10 results from the server and create the list.
+async function getSearchResults() {
+  const searchTerm = searchInput.value;
+  const endpoint = `https://stock-exchange-dot-full-stack-course-services.ew.r.appspot.com/api/v3/search?query=${searchTerm}&limit=10&exchange=NASDAQ`;
+
+  const response = await fetch(endpoint);
+  const data = await response.json();
+
+  searchResults.innerHTML = "";
+  for (let i = 0; i < data.length; i++) {
+    const dataSpecifics = data[i];
+
+    searchResults.innerHTML += `<li class="company-result"><a href="company.html?symbol=${dataSpecifics.symbol}"  target="_blank" class="a-href-style"><span class="symbol">${dataSpecifics.symbol}</span> - <span class="name">${dataSpecifics.name}</span></a></li>`;
+  }
+  disableSpinner();
+  searchInput.value = "";
+}
+
+//My main function.
+function mainFunction() {
+  enableSpinner();
+  getSearchResults();
+}
+
+// Enter key function..
+function handleInputUp(e) {
+  if (e.keyCode === 13) {
+    enableSpinner();
+    getSearchResults();
+  }
+}
+searchButton.addEventListener("click", mainFunction);
+searchInput.addEventListener("keyup", handleInputUp);
